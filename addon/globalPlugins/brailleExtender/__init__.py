aaclause/BrailleExtender

commit caccc2708ab518ac815b42daf8bc4186798f8c52

Avoid overlapping beeps (e.g.: when 'CTRL+alt' is performed at the same time)

addon/globalPlugins/brailleExtender/__init__.py
@@ -1073,41 +1073,41 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		if config.conf["brailleExtender"]["modifierKeysFeedback"] in [configBE.CHOICE_speech, configBE.CHOICE_speechAndBraille]:
 			speech.speakMessage(keyLabels.getKeyCombinationLabel('+'.join([m for m in self.modifiers])))
 
-	def toggleModifier(self, modifier):
+	def toggleModifier(self, modifier, beep = True):
 		if modifier.lower() not in ["alt","control","nvda","shift","windows"]:
 			return
 		if nativeModifiers: modifiers = brailleInput.handler.currentModifiers
 		else: modifiers = self.modifiers
 		if modifier not in modifiers:
 			modifiers.add(modifier)
-			if config.conf["brailleExtender"]["beepsModifiers"]: tones.beep(275, 50)
+			if beep and config.conf["brailleExtender"]["beepsModifiers"]: tones.beep(275, 50)
 		else:
 			modifiers.discard(modifier)
-			if config.conf["brailleExtender"]["beepsModifiers"]: tones.beep(100, 100 if len(modifiers) > 0 else 200)
+			if beep and config.conf["brailleExtender"]["beepsModifiers"]: tones.beep(100, 100 if len(modifiers) > 0 else 200)
 		if len(modifiers) == 0: self.clearModifiers(True)
 
 	def script_ctrl(self, gesture=None, sil=True):
-		self.toggleModifier("control")
+		self.toggleModifier("control", sil)
 		if sil: self.getActualModifiers()
 		return self.initCombKeys()
 
 	def script_nvda(self, gesture=None):
-		self.toggleModifier("nvda")
+		self.toggleModifier("nvda", sil)
 		self.getActualModifiers()
 		return self.initCombKeys()
 
 	def script_alt(self, gesture=None, sil=True):
-		self.toggleModifier("alt")
+		self.toggleModifier("alt", sil)
 		if sil: self.getActualModifiers()
 		return self.initCombKeys()
 
 	def script_win(self, gesture=None, sil=True):
-		self.toggleModifier("windows")
+		self.toggleModifier("windows", sil)
 		if sil: self.getActualModifiers()
 		return self.initCombKeys()
 
 	def script_shift(self, gesture=None, sil=True):
-		self.toggleModifier("shift")
+		self.toggleModifier("shift", sil)
 		if sil: self.getActualModifiers()
 		return self.initCombKeys()
 

commit 99723c2809939d21b10020abd2937e53a01b486a

Bug fix (regression from caccc27) with the NVDA modifier key

addon/globalPlugins/brailleExtender/__init__.py
@@ -1125,7 +1125,7 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		return self.initCombKeys()
 
 	def script_nvda(self, gesture=None):
-		self.toggleModifier("nvda", sil)
+		self.toggleModifier("nvda")
 		self.getActualModifiers()
 		return self.initCombKeys()
 
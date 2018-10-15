24774e318c54b75d0c3cab3a5e3f7ad93cc1b957
André-Abush CLAUSE <dev@andreabc.net>
2018-10-15 05:05:26 +0200

    Fix(autoscroll feature): don't hide cursor if autoscroll fails to start

--- addon/globalPlugins/brailleExtender/__init__.py
+++ addon/globalPlugins/brailleExtender/__init__.py
@@ -675,13 +675,15 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 				speech.speakMessage(_("Autoscroll stopped"))
 			config.conf["braille"]["showCursor"] = self.backupShowCursor
 		else:
+			self.autoScrollTimer = wx.PyTimer(self.autoScroll)
+			try: self.autoScrollTimer.Start(config.conf["brailleExtender"]["autoScrollDelay_%s" % configBE.curBD])
+			except BaseException as e:
+				log.error("%s | %s" % (config.conf["brailleExtender"]["autoScrollDelay_%s" % configBE.curBD], e))
+				ui.message(_("Unable to start autoscroll. More info in NVDA log"))
+				return
 			self.backupShowCursor = config.conf["braille"]["showCursor"]
 			config.conf["braille"]["showCursor"] = False
-			self.autoScrollTimer = wx.PyTimer(self.autoScroll)
-			self.autoScrollTimer.Start(
-				config.conf["brailleExtender"]["autoScrollDelay_%s" % configBE.curBD])
 		self.autoScrollRunning = not self.autoScrollRunning
-		return
 	script_autoScroll.__doc__ = _("Enable/disable autoscroll")
 
 	def autoScroll(self):

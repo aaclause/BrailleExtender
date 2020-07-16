5d19d872e29c89b0adc6382ef7ae52a923a895a3
André-Abush Clause <dev@andreabc.net>
2020-07-16 09:34:54 +0200

    Fixup for link report

--- addon/globalPlugins/brailleExtender/documentFormatting.py
+++ addon/globalPlugins/brailleExtender/documentFormatting.py
@@ -298,7 +298,7 @@ def getFormatFieldBraille(field, fieldCache, isAtStart, formatConfig):
 		link=field.get("link")
 		oldLink=fieldCache.get("link")
 		if link and link != oldLink:
-			textList.append(controlTypes.roleLabels[controlTypes.ROLE_LINK])
+			textList.append(braille.roleLabels[controlTypes.ROLE_LINK])
 
 	start_tag_list = []
 	end_tag_list = []

d000cbda5110b8926ffaa1e1a3144c7c96400acc
Emil Hesmyr <emilhe@viken.no>
2021-07-02 21:31:16 +0200

    Fix for alpha versions

--- addon/globalPlugins/brailleExtender/speechmode.py
+++ addon/globalPlugins/brailleExtender/speechmode.py
@@ -51,8 +51,15 @@ def speak(
 	index = len(speechList) - 1
 	showSpeech(index)
 
+if hasattr(speech, "speech"):
+	#to insure the speech function works correctly if another add-on creates a speech attribute on the speech module
+	if hasattr(speech.speech, "speak") and isinstance(speech.speech, type(speech)):
+		speech.speech.speak = speak
+	else:
+		speech.speak = speak
+else:
+	speech.speak = speak
 
-speech.speak = speak
 oldScrollBack = braille.BrailleBuffer.scrollBack
 
 
@@ -87,7 +94,7 @@ oldBrailleMessage = braille.handler.message
 
 
 def newBrailleMessage(*args, **kwargs):
-	if braille.handler.getTether != "speech":
+	if braille.handler.getTether() != "speech":
 		oldBrailleMessage(*args, **kwargs)
 
 
